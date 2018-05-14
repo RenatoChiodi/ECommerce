6aba2a1bdde70ebec3bd0a97d6bfc002b2833eda
Renato Davi Chiodi <renatod.chiodi@gmail.com>
2018-05-14 19:13:42 -0300

    Adicionar arquivos de projeto.

---
+++ ECommerce/Models/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        [Display(Name = "Cidade")]
+        public int CityId { get; set; }
+
+        [Display(Name = "Cidade")]
+        [Required(ErrorMessage = "O Campo Nome é Obrigatório!")]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Departamento")]
+        [Range(1,Double.MaxValue, ErrorMessage ="Selecione um Departamento!")]
+        public int DepartamentsId { get; set; }
+
+        public virtual Departaments Departament { get; set; }
+    }
+}
---
+++ ECommerce/Models/Departaments.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Departaments
+    {
+        [Key]
+        [Display(Name = "Departamento")]
+        public int DepartamentsId { get; set; }
+
+        [Required(ErrorMessage ="O Campo Nome é Obrigatório!")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
